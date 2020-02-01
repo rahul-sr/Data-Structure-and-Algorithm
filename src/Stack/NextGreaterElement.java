f5b897de9d604a5b67960dd53c23ea1e3136962e
rahulsrivastava <vnsrahulsri@gmail.com>
2020-02-01 19:13:29 +0530

    Next greater element

---
+++ src/Stack/NextGreaterElement.java
@@ -0,0 +1,11 @@
+package Stack;
+
+public class NextGreaterElement {
+    public static void main(String args[]) throws Exception {
+
+    }
+
+    public static int nextGreater() {
+        return 0;
+    }
+}
